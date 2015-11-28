e27ded3428e03182c3d65cb2a57678fa7fd2e6c3
Pidor <dmitrytsgn@gmail.com>
2015-11-29 03:37:46 +0400

    fancy sections menu

--- Assets/Scripts/SectionsMenuSetUp.cs
+++ Assets/Scripts/SectionsMenuSetUp.cs
@@ -9,9 +9,12 @@ public class SectionsMenuSetUp : MonoBehaviour {
     //hardcode hardcode hardcode
     private string[] sections = { "linear", "power", "root" };
 
+    public GUISkin SectionsMenuSkin;
+
     public Texture linearTexture;
     public Texture powerTexture;
     public Texture rootTexture;
+    private GUIContent content = new GUIContent();
 
     private Dictionary<string, Texture> icons = new Dictionary<string, Texture>();
     private Dictionary<string, string> functions = new Dictionary<string, string>();
@@ -33,13 +36,17 @@ public class SectionsMenuSetUp : MonoBehaviour {
 
     void OnGUI()
     {
+        GUI.skin = SectionsMenuSkin;
+
         var buttonSize = (new Vector3(Screen.width / 3f, Screen.width / 3f, 0f));
         for (int i = 0; i < sections.Length; ++i)
         {
+            content.image = icons[sections[i]];
+            content.text = sections[i];
 
             if (GUI.Button(new Rect(Screen.width  * 0.25f - buttonSize.x / 2 + Screen.width / 2 * (1 - (i+1) % 2),
                 Screen.width * 0.25f - buttonSize.x / 2 + buttonSize.y * 1.2f * (float)Math.Round((double)i / 2), 
-                buttonSize.x, buttonSize.y), icons[sections[i]]))
+                buttonSize.x, buttonSize.y), content))
             {
                 ScenesParameters.Section = sections[i];
 
